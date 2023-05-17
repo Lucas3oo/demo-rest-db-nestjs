1c99b1f05dad9af68473f8eb6d9c9331e9be4575
Lucas Persson <Lucas3oo@users.noreply.github.com>
2023-05-17 18:55:49 +0200

    fix formating and add sonarlint

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -2,15 +2,14 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { BookService } from './book.service';
 import { BookEntity } from './book.entity';
 
-@Controller('api/v1')
+@Controller('api/v1/books')
 export class BookController {
   constructor(private readonly service: BookService) {}
 
   /*
-   * If there isn't any match then the framework will return 200 OK which is a bit funny.
+   * If there isn't any match then the framework will return 200 OK.
    */
-
-  @Get('/books')
+  @Get()
   readAll(
     @Query('title') titel: string,
     @Query('isbn') isbn: string,
